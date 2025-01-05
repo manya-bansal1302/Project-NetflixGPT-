e35ba5c2952c4359bdae1272e3864bbbfd5291ec
Manya Bansal <manya1262.becse24@chitkara.edu.in>
2025-01-05 16:56:57 +0530

    sign in / sign up form && login/header

--- README.md
+++ README.md
@@ -2,6 +2,9 @@
 
 -Create React App
 -Configured TailwindCSS in our App 
+-Header
+-Login Form
+-Sign up Form
 
 # Features
 
--- src/App.js
+++ src/App.js
@@ -1,11 +1,8 @@
-
-import './App.css';
+import Body from './Components/Body';
 
 function App() {
   return (
-    <div className="text-3xl font-bold text-red-400">
-      Let's Build Netflix-gpt
-    </div>
+    <Body/>
   );
 }
 
